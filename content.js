83d1d06b953e1fa8fe01814d32ad78e3daa81a63
Anthony Brinkhuis <tbrinkhuis@dfsolution.com>
2022-11-22 14:17:29 -0700

    Date handling
    converting date properly and added "days ago "

--- content.js
+++ content.js
@@ -1,12 +1,10 @@
 const collection = document.getElementsByClassName("time-since");
-// user setpoints
-var timeOffset = -7;
-var timeZone = "MST";
 
-var dateString = "";
 var dateArray = "";
-var timeString = "";
-var newHour = "";
+var dateString = "";
+var dateFinalArray = "";
+var daysAgo = "";
+
 for (let i = 0; i < collection.length; i++) {
 
   // console.log(collection[i]);
@@ -14,14 +12,13 @@ for (let i = 0; i < collection.length; i++) {
   // Original time format below
   // Thu, 03 Nov 2022 18:22:03 UTC
   dateArray = collection[i].getAttribute('data-content').split(" ");
-  timeString = dateArray[4].split(":");
-  newHour = parseInt(timeString[0]);
-  newHour = newHour + timeOffset;
-  timeString[0] = newHour;
-  dateArray[4] = timeString.join(':');
-  dateArray[5] = timeZone;
+  daysAgo = collection[i].textContent;
+
+  dateArray[0] = "";
   dateString = dateArray.join(' ');
-  //dateString = collection[i].getAttribute('data-content');
-  collection[i].textContent = dateString;
+  dateString = new Date(dateString).toLocaleString();
+  dateFinalArray = dateString.split(" (");
+  console.log(dateFinalArray[0] + " (" + daysAgo + ")");
+  collection[i].textContent = (dateFinalArray[0] + " (" + daysAgo + ")");
 
 }
