ef36674116621a0e4ca67a5dde13a239711c8d48
Anthony Brinkhuis <tbrinkhuis@dfsolution.com>
2022-11-22 14:18:50 -0700

    Delete content_non_working.js

--- content_non_working.js
+++
@@ -1,5 +0,0 @@
-var collection = document.getElementsByClassName("time-since");
-
-for (let i = 0; i < collection.length; i++) {
-  collection[i].innerHTML = collection[i].getAttribute("title").toString();
-}
